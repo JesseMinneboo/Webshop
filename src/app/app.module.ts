700cfb3c7368642e814e36b1010f544ab4296644
Jesse Minneboo <47326206+JesseMinneboo@users.noreply.github.com>
2020-01-03 22:14:53 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './shared/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/loginModal/login.component';
+import { RegisterComponent } from './shared/registerModal/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ShoppingcartComponent } from './pages/shoppingcart/shoppingcart.component';
+import { WarningModalComponent } from './shared/warning-modal/warning-modal.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'shoppingcart', component: ShoppingcartComponent }
+  // { path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    StoreComponent,
+    ShoppingcartComponent,
+    WarningModalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
